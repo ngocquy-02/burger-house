12c54e347976edd5634dea8727b7a710c2f3779f
Ngọc Quý <90969371+ngocquy-02@users.noreply.github.com>
2022-09-05 18:36:44 +0700

    fix bug

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let navbar = document.querySelector('.header nav');
 let menuBtn = document.querySelector('#menu-btn');
+let scrollTop = document.querySelector('.scroll-top');
 
 menuBtn.onclick = () =>{
    menuBtn.classList.toggle('fa-times');
    navbar.classList.toggle('active');
 };
+
+window.onscroll = () =>{
+
+   if(window.scrollY > 250){
+       scrollTop.style.display = 'initial';
+   }else{
+       scrollTop.style.display = 'none';
+   }
+
+}
